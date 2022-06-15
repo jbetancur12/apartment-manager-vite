fb0db0773b6decfee9f84f6ffef1d7a5e3f600a4
Jorge Betancur <jabetancur12@gmail.com>
2022-06-15 02:11:09 +0000

    feat(menu.tsx): added active class to sidebar menu

--- src/Components/Menu/Menu.tsx
+++ src/Components/Menu/Menu.tsx
@@ -7,45 +7,69 @@ import {
 import { AiOutlineHome } from 'react-icons/ai'
 import { RiParentLine, RiSettings4Line } from 'react-icons/ri'
 import './Menu.scss'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const Menu = (): JSX.Element => {
   return (
-    <div className="Menu">
-      <a href="/" className="Menu-brand">
+    <div className='Menu'>
+      <a href='/' className='Menu-brand'>
         <MdApartment />
-        <span className="Menu-brand-text">Apartment Manager</span>
+        <span className='Menu-brand-text'>Apartment Manager</span>
       </a>
-      <div className="Menu-items">
-        <div className="Menu-items-content">
-          <Link to="/" className="Menu-items-item">
+      <div className='Menu-items'>
+        <div className='Menu-items-content'>
+          <NavLink
+            to='/'
+            className={({ isActive }) =>
+              isActive
+                ? 'Menu-items-item Menu-items-item_active'
+                : 'Menu-items-item'}
+          >
             <AiOutlineHome />
-            <span className="Menu-items-text">Dashboard</span>
-          </Link>
-          <Link to="/tenants" className="Menu-items-item">
+            <span className='Menu-items-text'>Dashboard</span>
+          </NavLink>
+          <NavLink
+            to='/tenants'
+            className={({ isActive }) =>
+              isActive
+                ? 'Menu-items-item Menu-items-item_active'
+                : 'Menu-items-item'}
+          >
             <RiParentLine />
-            <span className="Menu-items-text">Tenants</span>
-          </Link>
-          <a href="/" className="Menu-items-item">
+            <span className='Menu-items-text'>Tenants</span>
+          </NavLink>
+          <NavLink
+            to='/apartments'
+            className={({ isActive }) =>
+              isActive
+                ? 'Menu-items-item Menu-items-item_active'
+                : 'Menu-items-item'}
+          >
             <MdApartment />
-            <span className="Menu-items-text">Apartments</span>
-          </a>
-          <a href="/" className="Menu-items-item">
+            <span className='Menu-items-text'>Apartments</span>
+          </NavLink>
+          <NavLink
+            to='/balance'
+            className={({ isActive }) =>
+              isActive
+                ? 'Menu-items-item Menu-items-item_active'
+                : 'Menu-items-item'}
+          >
             <MdAccountBalance />
-            <span className="Menu-items-text">Balance</span>
-          </a>
+            <span className='Menu-items-text'>Balance</span>
+          </NavLink>
         </div>
       </div>
-      <div className="Menu-items">
-        <div className="Menu-items-content">
-          <a href="/" className="Menu-items-item">
+      <div className='Menu-items'>
+        <div className='Menu-items-content'>
+          <a href='/' className='Menu-items-item'>
             <RiSettings4Line />
-            <span className="Menu-items-text">Settings</span>
+            <span className='Menu-items-text'>Settings</span>
           </a>
-          <a href="/" className="Menu-items-item">
+          <a href='/' className='Menu-items-item'>
             <MdOutlineNotificationsNone />
-            <span className="Menu-items-text">Notifications</span>
-            <span className="Menu-items-notifications" />
+            <span className='Menu-items-text'>Notifications</span>
+            <span className='Menu-items-notifications' />
           </a>
         </div>
       </div>
